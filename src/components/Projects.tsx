4c9667e17987bebebb2b222efe64fe18eec3d663
linkoten <catto.francois@hotmail.fr>
2025-02-25 17:35:51 +0100

    on modifie légèrement le portfolio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Logo from "../../public/LogoVert.jpg";
-import studioGhibliLogo from "../../public/studio-ghibli.png";
 import Paleolitho from "../../public/Paleolitho.png";
-import FullStack from "../../public/fullStackJuillet2024.png";
 import Image from "next/image";
 import Link from "next/link";
+import Tarot from "../../public/Capture8.png";
 
 const Projects = () => {
   return (
@@ -18,6 +17,60 @@ const Projects = () => {
         </h2>
       </div>
       <ul className="group/list">
+        <li className="mb-12">
+          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+            <div className="z-10 sm:order-2 sm:col-span-6">
+              <h3>
+                <Link
+                  className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                  href="https://tarot-production-f5d1.up.railway.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  aria-label="Build a Spotify Connected App (opens in a new tab)"
+                >
+                  <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                  <span>
+                    Jeu de Tarot
+                    <span className="inline-block">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                  </span>
+                </Link>
+              </h3>
+              <p className="mt-2 text-sm leading-normal">
+                Cette application est un jeu de tarot en ligne complet,
+                respectant les règles officielles du tarot français. Ce projet
+                ambitieux m&apos;a permis de mettre en œuvre mes compétences en
+                développement web full-stack et de me familiariser avec des
+                technologies modernes telles que Next.js, Prisma, Redux et les
+                WebSockets.
+              </p>
+            </div>
+            <Image
+              alt="Jeu de tarot en ligne respectant les règles officielles"
+              loading="lazy"
+              width="200"
+              height="48"
+              decoding="async"
+              data-nimg="1"
+              className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+              src={Tarot}
+            />
+          </div>
+        </li>
         <li className="mb-12">
           <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
             <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
@@ -64,7 +117,7 @@ const Projects = () => {
               </p>
             </div>
             <Image
-              alt="Build a Spotify Connected App Newline course marketing card"
+              alt="Site e-commerce spécialisé dans la vente de fossilles Français et Marocains "
               loading="lazy"
               width="200"
               height="48"
@@ -117,7 +170,7 @@ const Projects = () => {
               </p>
             </div>
             <Image
-              alt="Build a Spotify Connected App Newline course marketing card"
+              alt="Site Vitrine pour un restaurant situé à Quimper"
               loading="lazy"
               width="200"
               height="48"
